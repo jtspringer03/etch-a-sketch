08cc21f7004d4a0e9c07508d601701cd2cabd94c
jtspringer03 <jtspringer03@yahoo.com>
2022-12-30 17:30:26 +0800

    complete final javascript

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let color = "black";
+
+let btnSelect = document.querySelector(".btn-danger");
+btnSelect.addEventListener("click", function () {
+  let size = getSize();
+  createBoard(size);
+});
+
 function createBoard(size) {
   let board = document.querySelector(".board");
 
@@ -8,7 +16,36 @@ function createBoard(size) {
 
   for (let i = 0; i < numDivs; i++) {
     let div = document.createElement("div");
-    board.style.backgroundColor = "yellow";
+    div.addEventListener("mouseover", colorDiv);
     board.insertAdjacentElement("beforeend", div);
   }
+}
+
+function getSize() {
+  let input = prompt("What size (1 - 100) board would you like?");
+  let message = document.querySelector("#message");
+
+  if (input == "" || input <= 0 || input > 100) {
+    message.textContent = "Please provide a number between 1 and 100";
+  } else {
+    message.textContent = "You may start Etch-A-Sketching";
+    return input;
+  }
+}
+
+function colorDiv() {
+  if (color == "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = "black";
+  }
+}
+
+function setColor(colorChoice) {
+  color = colorChoice;
+}
+
+function resetBoard() {
+  let divs = document.querySelectorAll("div");
+  divs.forEach((div) => (div.style.backgroundColor = "white"));
 }
